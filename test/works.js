981bbb1f8aa7b3ac94fed9ea70507200fc6a8797
geoffreyTools <geoffreyTools@users.noreply.github.com>
2021-02-17 23:33:53 +0100

    add proper tests

--- test/works.js
+++ test/works.js
@@ -1,24 +1,58 @@
-const avaBabel = require('../dist/ava-babel-wrapper.js');
+import avaBabel from 'ava-babel-wrapper';
 
-const foo = () => 'foo';
-const bar = () => 'bar';
-
-const scope = { foo, bar };
-
-const plugins = [
-    () => ({
-        visitor: {
-            Identifier(path) {
-                if (path.isIdentifier({ name: 'foo' })) 
-                    path.node.name = 'bar';
-            },
+const plugin = () => ({
+    visitor: {
+        Identifier(path) {
+            if (path.isIdentifier({ name: 'foo' })) 
+                path.node.name = 'bar';
+            else if (path.isIdentifier({ name: 'baz' }))
+                throw new Error('baz is a forbidden identitifer')
         },
-    })
-];
+    },
+});
 
-const test = avaBabel(plugins, scope);
+const foo = 0;
+const bar = 1;
 
-test('`foo()` should be turned to `bar()`', (t, code) => {
-    t.is(foo(), 'bar');
+const test = avaBabel([plugin], { foo, bar });
+
+test('foo should be turned to bar', (t, code) => {
+    t.is(foo, bar);
     t.log(code);
+});
+
+test('identifier `baz` should throw', 
+    t => {
+        const baz = 'baz';
+    }, (t, json) => {
+        const { type, message } = JSON.parse(json);
+        t.is(type, 'compile-time error');
+        t.is(message, 'unknown: baz is a forbidden identitifer');
+    }
+);
+
+test.todo('some test to be written later');
+
+test.skip('some test to be skipped', t => {
+    t.not(foo, 42);
+});
+
+test.failing('should fail', (t, code) => {
+    t.is(foo, 'foo');
 })
+
+test.serial('should come 1st', (t, code) => {
+    t.is(foo, bar);
+});
+
+test.serial('should come 2nd', (t, code) => {
+    t.is(foo, bar);
+});
+
+test.serial('should come 3rd', (t, code) => {
+    t.is(foo, bar);
+});
+
+// test.only('should be the only test run', (t, code) => {
+//    t.is(foo, bar);
+// });
